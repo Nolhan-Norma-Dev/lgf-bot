04397ee57c1cf3ce9090d8921717566cac42b57e
lagriculteur-normand <63206684+lagriculteur-normand@users.noreply.github.com>
2020-05-11 13:32:44 +0200

    Update avatar.js

--- Commandes/Everyone Commandes/Jeux/avatar.js
+++ Commandes/Everyone Commandes/Jeux/avatar.js
@@ -7,11 +7,12 @@ module.exports.run = async(bot, message, args) => {
     let mentionnedUser = message.mentions.members.first() || message.member;
     let embed =  new Discord.MessageEmbed()
 
-        .setTitle("__Avatar__")
+        .setTitle("__Avatar :__")
+        .setAuthor(message.author.username, message.author.avatarURL())
         .setColor("RANDOM")
         .setDescription(`[Lien de l'image](${mentionnedUser.user.displayAvatarURL()})`)
         .setImage(`${mentionnedUser.user.displayAvatarURL()}`)
-        .setFooter(`${bot.user.username} by UnNainCassable| Demandé par ${message.author.tag}`);
+        .setFooter(`© 2020 : Bot et message créé par l'agriculteur normand | Tous droits réservés.`, `https://zupimages.net/up/20/13/omgq.png`);
 
         waiting.edit(embed).catch(console.error);
 };
